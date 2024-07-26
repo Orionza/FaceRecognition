394e6d19083b48ab1d5b4695f2825b371c7786a9
Orionza <emrecelik3487@gmail.com>
2024-07-26 17:15:05 +0300

    Initial commit

---
+++ FaceRecognitionn.py
@@ -0,0 +1,55 @@
+import threading
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+
+reference_img_path = "emre.jpeg"  # use your own image here
+reference_img = cv2.imread(reference_img_path)
+
+if reference_img is None:
+    print(f"Error: Unable to load image from path {reference_img_path}")
+else:
+    print(f"Image {reference_img_path} loaded successfully")
+
+face_match = False
+
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
+
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+        if face_match:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow('video', frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
